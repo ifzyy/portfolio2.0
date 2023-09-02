45cb23d88f0817ca4ce7d0ee0626eac2186f3508
Johnson Emmanuel <johnsonnifemi8@gmail.com>
2023-09-02 22:40:36 +0100

    update app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/footer.css'
 const Footer = () => {
+  const date = new Date().getFullYear()
   return (
     <div>
-          <div className="footer"><span>&copy; 2023</span>  Designed by Johnson</div>
+          <div className="footer"><span>&copy; {date}</span>  Designed by Johnson</div>
     </div>
   )
 }
